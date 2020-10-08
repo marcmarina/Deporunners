be1c6f03fe79c5edbab2be1583597ebe9dc87938
Marc Marina Miravitlles <marc.marina.miravitlles@gmail.com>
2020-10-08 12:27:06 +0200

    Added Event Pagination

--- app/screens/EventsScreen.tsx
+++ app/screens/EventsScreen.tsx
@@ -9,6 +9,7 @@ import Event from '../interfaces/Event';
 const EventsScreen: FC = () => {
   const [events, setEvents] = useState<Event[]>();
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     retrieveData();
@@ -17,11 +18,35 @@ const EventsScreen: FC = () => {
   const retrieveData = async () => {
     try {
       setLoading(true);
-      const { data } = await client.get('/event');
+      const { data } = await client.get(`/event?page=1&limit=10`);
       setEvents(data);
       setLoading(false);
     } catch (ex) {
       console.log(ex);
+    }
+  };
+
+  const retrieveMore = async () => {
+    try {
+      setLoading(true);
+
+      const { data } = await client.get(`/event?page=${page + 1}&limit=10`);
+
+      if (events && data.length > 0) {
+        const newEvents = [...events, ...data];
+        setEvents(newEvents);
+      }
+      setPage(page + 1);
+
+      setLoading(false);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  const increasePage = () => {
+    if (!loading) {
+      retrieveMore();
     }
   };
 
@@ -32,10 +57,15 @@ const EventsScreen: FC = () => {
         keyExtractor={item => item._id}
         contentContainerStyle={styles.list}
         showsVerticalScrollIndicator={false}
-        onRefresh={() => retrieveData()}
+        onRefresh={() => {
+          setPage(1);
+          retrieveData();
+        }}
         refreshing={loading}
         ItemSeparatorComponent={() => <View style={styles.separator} />}
         renderItem={({ item }) => <EventListItem event={item} />}
+        onEndReached={increasePage}
+        onEndReachedThreshold={0.2}
       />
     </Screen>
   );
